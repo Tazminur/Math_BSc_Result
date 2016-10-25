6f633da56f226ca0f5a665ecde617d92cec62eff
Tazminur <tazminur.ru@gmail.com>
2016-10-25 20:32:55 +0600

    modal updated

--- header.php
+++ header.php
@@ -31,14 +31,14 @@
         		<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
         		<h4 class="modal-title text-center" id="loginModalLabel">USER AUTHENTICATION</h4>
       		</div>
-      		<div class="modal-body">
+      		<div class="modal-body" style="padding-bottom:40px">
       			<div class="text-center">
 	      			<div role="tabpanel" class="login-tab">
 					  	<!-- Nav tabs -->
 					  	<ul class="nav nav-tabs" role="tablist">
 					    	<li role="presentation" class="active"><a id="signin-taba" href="#home" aria-controls="home" role="tab" data-toggle="tab">Sign In</a></li>
 					    	<li role="presentation"><a id="signup-taba" href="#profile" aria-controls="profile" role="tab" data-toggle="tab">Sign Up</a></li>
-					    	<li role="presentation"><a id="forgetpass-taba" href="#forget_password" aria-controls="forget_password" role="tab" data-toggle="tab">Forget Password</a></li>
+					    	<!-- <li role="presentation"><a id="forgetpass-taba" href="#forget_password" aria-controls="forget_password" role="tab" data-toggle="tab">Forget Password</a></li> -->
 					  	</ul>
 					
 					  	<!-- Tab panes -->
@@ -64,20 +64,6 @@
 								  	</div>
 						  			<button type="button" id="login_btn" class="btn btn-block bt-login" data-loading-text="Signing In....">Login</button>
 						  			<div class="clearfix"></div>
-						  			<div class="login-modal-footer">
-						  				<div class="row">
-											<div class="col-xs-8 col-sm-8 col-md-8">
-												<i class="fa fa-lock"></i>
-												<a href="javascript:;" class="forgetpass-tab"> Forgot password? </a>
-											
-											</div>
-											
-											<div class="col-xs-4 col-sm-4 col-md-4">
-												<i class="fa fa-check"></i>
-												<a href="javascript:;" class="signup-tab"> Sign Up </a>
-											</div>
-										</div>
-						  			</div>
 								</form>
 					    	</div>
 					    	<div role="tabpanel" class="tab-pane" id="profile">
@@ -101,20 +87,6 @@
 								  	</div>
 						  			<button type="button" id="register_btn" class="btn btn-block bt-login" data-loading-text="Registering....">Register</button>
 									<div class="clearfix"></div>
-									<div class="login-modal-footer">
-						  				<div class="row">
-											<div class="col-xs-8 col-sm-8 col-md-8">
-												<i class="fa fa-lock"></i>
-												<a href="javascript:;" class="forgetpass-tab"> Forgot password? </a>
-											
-											</div>
-											
-											<div class="col-xs-4 col-sm-4 col-md-4">
-												<i class="fa fa-check"></i>
-												<a href="javascript:;" class="signin-tab"> Sign In </a>
-											</div>
-										</div>
-						  			</div>
 								</form>
 					    	</div>
 					    	<div role="tabpanel" class="tab-pane text-center" id="forget_password">
